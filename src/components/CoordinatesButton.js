e2fc9307b4fdc015f44102182cf46891eb1b0c0c
github username <sharnellgrant@gmail.com>
2021-10-19 23:25:57 -0400

    commit tree 3a1c677bd859db7dfcf42c29cc70aeedfc44a55f

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+ 
+
+class CoordinatesButton extends React.Component {
+    
+    eventHandler = (event) => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    }
+    render() {
+        return <button onClick={this.eventHandler}> </button>;
+    }
+
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
 // Code DelayedButton Component Here
+
+import React from 'react'
+ 
+
+class DelayedButton extends React.Component {
+    
+ render() {
+    return <button onClick={this.props}> </button>;
+ }
+    
+}
+
+export default DelayedButton;
